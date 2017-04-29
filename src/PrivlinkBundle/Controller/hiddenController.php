04a9c9202144a843f1320d4e71ff5f242298b3d0
CupCoffe <k.oleh@yahoo.com>
2017-04-30 02:09:09 +0300

    new controller to select text from base with hash

---
+++ src/PrivlinkBundle/Controller/hiddenController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace PrivlinkBundle\Controller;
+
+use PrivlinkBundle\Entity\privlink;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Hidden controller.
+ *
+ * @Route("hidden")
+ */
+
+class hiddenController extends Controller
+{
+
+    public function indexAction($hash)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $note = $em->createQueryBuilder('privlink')
+            ->select('privlink')
+            ->from('PrivlinkBundle:privlink','privlink')
+            ->andWhere('privlink.hash IN (:hash)')
+            ->setParameter('hash', $hash)
+            ->getQuery();
+        $text = $note->getResult();
+        return $this->render('PrivlinkBundle:privlink:hidden.html.twig', array(
+            'privlinks' => $text,
+        ));
+
+    }
+
+
+}
+
+
+
+?>
